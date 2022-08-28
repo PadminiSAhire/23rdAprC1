1d9db539a11ab8a6056f99479134e9343e9d8fea
MINAL & LAKSHITA <MINAL & LAKSHITA@LAPTOP-JUARLNE1>
2022-08-28 14:33:33 +0530

    [Padmini] 1st changes in project

---
+++ src/sample1/demo1.java
@@ -0,0 +1,31 @@
+package sample1;
+
+public class demo1 {
+
+public static void main(String[] args) {
+		
+		
+System.out.println("Hiiii");		
+		
+		
+System.out.println("Hello");		
+		
+		
+		
+		
+		
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
